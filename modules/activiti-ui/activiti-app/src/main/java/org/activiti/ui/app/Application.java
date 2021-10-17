01ca72faf6468b3c02fadc18cdaf524ae0a4fca5
wenping <410619028@qq.com>
2021-10-17 16:32:49 +0800

    修改

---
+++ modules/activiti-ui/activiti-app/src/main/java/org/activiti/ui/app/Application.java
@@ -0,0 +1,58 @@
+package org.activiti.ui.app;
+
+import org.activiti.app.conf.ApplicationConfiguration;
+import org.activiti.app.servlet.ApiDispatcherServletConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootApplication
+@Import({ApplicationConfiguration.class})
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean apiDispatcher() {
+        DispatcherServlet api = new DispatcherServlet();
+        api.setContextClass(AnnotationConfigWebApplicationContext.class);
+        api.setContextConfigLocation(ApiDispatcherServletConfiguration.class.getName());
+
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(api);
+        registrationBean.addUrlMappings("/api/*");
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.setAsyncSupported(true);
+        registrationBean.setName("api");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletRegistrationBean appDispatcher() {
+        DispatcherServlet app = new DispatcherServlet();
+        app.setContextClass(AnnotationConfigWebApplicationContext.class);
+        app.setContextConfigLocation(ApiDispatcherServletConfiguration.class.getName());
+
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(app);
+        registrationBean.addUrlMappings("/app/*");
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.setAsyncSupported(true);
+        registrationBean.setName("app");
+
+        return registrationBean;
+    }
+}
