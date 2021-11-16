902118da8ca5f822cd1552e56ed4a68b5c4395b7
wenping <410619028@qq.com>
2021-11-16 21:27:31 +0800

    完成 activiti-app 升级

--- modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/SecurityConfiguration.java
+++ modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/SecurityConfiguration.java
@@ -160,6 +160,9 @@ public class SecurityConfiguration {
 	                .antMatchers("/app/rest/idm/email-actions/*").permitAll()
 	                .antMatchers("/app/rest/idm/signups").permitAll()
 	                .antMatchers("/app/rest/idm/passwords").permitAll()
+					.antMatchers("/druid/**").authenticated()
+					.antMatchers("/actuator/**").authenticated()
+					.antMatchers("/manage/**").authenticated()
 	                .antMatchers("/app/**").authenticated();
 
 	        // Custom login form configurer to allow for non-standard HTTP-methods (eg. LOCK)
